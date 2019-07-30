e6a61f4ea952642daa1569082b6ff7a82ec25890
watermelo <80680489@qq.com>
2019-07-31 00:48:20 +0800

    part 1~2

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/watermelo/realtime-chat-go-react
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.0 // indirect
---
+++ frontend/src/api/index.js
@@ -0,0 +1,28 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+let connect = () => {
+    console.log("Attempting Connection...");
+
+    socket.onopen = () => {
+        console.log("Successfully Connected");
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+    };
+
+    socket.onclose = event => {
+        console.log("Socket Closed Connection: ", event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket Error: ", error);
+    };
+};
+
+let sendMsg = msg => {
+    console.log("sending msg: ", msg);
+    socket.send(msg);
+};
+
+export { connect, sendMsg };
